0df223fdde8146d6ba277eb2d343ec0bc221282c
thomasbroeren <thomasbroeren@outlook.com>
2026-04-23 10:54:28 +0200

    Add src exercise solution

---
+++ src/analysis.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 10:50:24 2026
+
+@author: thoma
+"""
+
---
+++ src/visualise.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 10:50:52 2026
+
+@author: thoma
+"""
+
